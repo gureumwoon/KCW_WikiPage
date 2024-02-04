e82b4d569b91813d4c73b74828443241f00140d4
gureumwoon <vfrvfr3221@gmail.com>
2024-02-04 18:57:00 +0900

    자주 사용하는 컬러 theme으로 설정해두기

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor: '#3080ff',
+        subColor: '#F3F3F5'
+      }
+    },
   },
   plugins: [],
 }
